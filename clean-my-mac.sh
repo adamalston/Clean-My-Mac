839865b02743c636de2d9036aa647be330c6f5a3
Adam <aalston9@gmail.com>
2020-06-27 16:55:40 -0400

    redirect stderr to the null file

--- clean-my-mac.sh
+++ clean-my-mac.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
 # user caches
-echo "Clear user cache files from ~/Library/Caches"
+echo "Clear user cache files"
 rm -rfv ~/Library/Caches/* > /dev/null 2>&1
 
 # user logs
-echo "Clear user log files from ~/Library/logs"
+echo "Clear user log files"
 rm -rfv ~/Library/logs/* > /dev/null 2>&1
 
 # system caches
-echo "Clear system cache files from /Library/Caches/*"
+echo "Clear system cache files"
 sudo rm -rfv /Library/Caches/* > /dev/null 2>&1
 
 # system logs
-echo "Clear system log files from /Library/logs/*"
+echo "Clear system log files"
 sudo rm -rfv /Library/logs/* > /dev/null 2>&1
-
-echo "Clear system log files from /var/log/*"
 sudo rm -rfv /var/log/* > /dev/null 2>&1
 
 # private system caches
-echo "Clear files from /private/var/folders/*"
+echo "Clear quicklook files"
 sudo rm -rfv /private/var/folders/* > /dev/null 2>&1
 
+# trash
 echo "Empty trash"
 sudo rm -rfv /Volumes/*/.Trashes > /dev/null 2>&1
 sudo rm -rfv ~/.Trash  > /dev/null 2>&1
